d06c87d809b582c0bbeab046e6b2ee4c78ed4b0e
hoangadamo <hoang.caoviet@adamosoft.com>
2024-09-10 15:11:18 +0700

    ES6 Iterator - Iterable

---
+++ Day1/javascript_advance/ES6_Iterator_iterable/Iterable.js
@@ -0,0 +1,6 @@
+var array = [1, 2, 3, 4];
+for (let x of array) {
+  console.log(x);
+}
+console.log(...array);
+
---
+++ Day1/javascript_advance/ES6_Iterator_iterable/Iterator.js
@@ -0,0 +1,6 @@
+let arr = ["a", "b", "c"];
+var iterator = arr[Symbol.iterator]();
+console.log(iterator.next()); //a
+console.log(iterator.next()); //b
+console.log(iterator.next()); //c
+console.log(iterator.next()); //undefined
