8aa649166961ec577bda351e5476176ceb668d75
hoangadamo <hoang.caoviet@adamosoft.com>
2024-10-07 13:49:25 +0700

    add swagger

--- NestJS/Schedule-Queue-Mail/practice/src/main.ts
+++ NestJS/Schedule-Queue-Mail/practice/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -8,6 +9,13 @@ const PORT = process.env.PORT || 3001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Loyalty System API')
+    .setDescription('API documentation for the Loyalty System')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(PORT);
   console.log('running on', PORT);
 }
